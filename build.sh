23f9fc41fc3c25e93039bdbb56e973b973ca287c
Axel Perez <31319618+axperez@users.noreply.github.com>
2023-03-20 18:52:55 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Change with your project ID and imagine name/tag
-IMAGE=gcr.io/my-project/my-image:1.0.1
+IMAGE=us-docker.pkg.dev/my-project/serverless-spark/my-image:1.0.1
 gsutil cp gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.13-0.28.1.jar build/.
 wget -P build/ https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-x86_64.sh
 # Build and push docker container image
